1d54818b57a2f159de32da9486ed4592abd04524
WabiDagim <wabidagim280@gmail.com>
2026-02-08 01:30:34 -1000

    db cases

--- db/DbConfig.js
+++ db/DbConfig.js
@@ -1,10 +1,18 @@
 const mysql2 = require('mysql2');
 require('dotenv').config();
+
 const db = mysql2.createPool({
     host: process.env.DB_HOST,
-     user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectionLimit: parseInt(process.env.DB_CONN_LIMIT, 10)
-})
+    port: process.env.DB_PORT || 4000,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    // TiDB Cloud REQUIRES this SSL block
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    },
+    connectionLimit: 10
+});
+
 module.exports = db.promise();
